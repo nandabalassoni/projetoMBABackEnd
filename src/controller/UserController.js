4ff8665ffda184058e9943b6480b7a5e264c6717
nandabalassoni <mariafernandabalassoni@hotmail.com>
2023-11-16 02:16:41 -0300

    creating method for user authenticate and update password

--- src/controller/UserController.js
+++ src/controller/UserController.js
@@ -17,4 +17,32 @@ class UserController {
         // Enviando uma resposta ao usuário
         res.status(201).json({ message: 'Usuário criado com sucesso' });
     }
+
+    async authenticate(req, res) {
+        const { email, senha } = req.body;
+        // Aqui você deve buscar o usuário no banco de dados
+        const user = await User.findOne({ email });
+        const match = await bcrypt.compare(senha, user.senha);
+        if (match) {
+            const token = jwt.sign({ id: user.id }, 'your-secret-key');
+            res.json({ token });
+        } else {
+            res.status(401).json({ message: 'Senha incorreta' });
+        }
+    }
+
+    async changePassword(req, res) {
+        const { email, oldPassword, newPassword } = req.body;
+        // Aqui você deve buscar o usuário no banco de dados
+        const user = await User.findOne({ email });
+        const match = await bcrypt.compare(oldPassword, user.senha);
+        if (match) {
+            const hashedPassword = await bcrypt.hash(newPassword, 10);
+            // Aqui você deve atualizar a senha do usuário no banco de dados
+            await User.updateOne({ email }, { senha: hashedPassword });
+        } else {
+            res.status(401).json({ message: 'Senha antiga incorreta' });
+        }
+    }
 }
+module.exports = new UserController();
